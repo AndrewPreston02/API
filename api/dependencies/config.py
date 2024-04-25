77b41e6545d363ec435c52b8a492ba533bed48b3
“Emma <emaries@uncc.edu>
2024-04-25 11:15:47 -0400

    Feat: I changed the db_password to a default

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "online_restaurant"
     db_port = 3306
     db_user = "root"
-    db_password = "37R4bq123!"
+    db_password = "password"
     app_host = "localhost"
     app_port = 8000
