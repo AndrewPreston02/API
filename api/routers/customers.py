0ed28920fec0b7ed5028f3a93d358cff88fd6992
“Emma <emaries@uncc.edu>
2024-04-29 14:28:39 -0400

    Feat: Updated customers.py in routers

--- api/routers/customers.py
+++ api/routers/customers.py
@@ -11,25 +11,25 @@ router = APIRouter(
 
 
 @router.post("/", response_model=schema.Customer)
-def create_customer(request: schema.CustomerCreate, db: Session = Depends(get_db)):
+def create(request: schema.CustomerCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
 
 
 @router.get("/", response_model=list[schema.Customer])
-def read_all_customers(db: Session = Depends(get_db)):
+def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
 
 @router.get("/{item_id}", response_model=schema.Customer)
-def read_one_customer(item_id: int, db: Session = Depends(get_db)):
+def read_one(item_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db, item_id=item_id)
 
 
 @router.put("/{item_id}", response_model=schema.Customer)
-def update_customer(item_id: int, request: schema.CustomerUpdate, db: Session = Depends(get_db)):
+def update(item_id: int, request: schema.CustomerUpdate, db: Session = Depends(get_db)):
     return controller.update(db=db, request=request, item_id=item_id)
 
 
 @router.delete("/{item_id}")
-def delete_customer(item_id: int, db: Session = Depends(get_db)):
+def delete(item_id: int, db: Session = Depends(get_db)):
     return controller.delete(db=db, item_id=item_id)
